9478a531591fa7df33aa8850f261063525ba6427
mihares <mihares@gmail.com>
2014-10-31 19:44:25 -0300

    Conn
    sqlserver

---
+++ DatosGlobales.vb
@@ -0,0 +1,352 @@
+﻿Imports System
+
+Namespace DatosGlobales
+    ''' <summary> 
+    ''' Clase abstracta de acceso a Datos. 
+    ''' </summary> 
+    Public MustInherit Class DatosGlobales
+#Region "Constructores"
+        Public Sub New()
+        End Sub
+#End Region
+#Region "Declaración de Atributos"
+        Protected mServidor As String = ""
+        Protected mBase As String = ""
+        Protected mUsuario As String = ""
+        Protected mPassword As String = ""
+        Protected mCadenaConexion As String = ""
+        Protected mConexion As System.Data.IDbConnection
+#End Region
+#Region "Propiedades"
+        ''' <summary> 
+        ''' Nombre del equipo servidor de datos. 
+        ''' </summary> 
+        Public Property Servidor() As String
+            Get
+                Return mServidor
+            End Get
+            Set(ByVal value As String)
+                mServidor = value
+            End Set
+        End Property
+        ''' <summary> 
+        ''' Nombre de la base de datos a utilizar. 
+        ''' </summary> 
+        Public Property Base() As String
+            Get
+                Return mBase
+            End Get
+            Set(ByVal value As String)
+                mBase = value
+            End Set
+        End Property
+        ''' <summary> 
+        ''' Nombre del Usuario de la BD. 
+        ''' </summary> 
+        Public Property Ususario() As String
+            Get
+                Return mUsuario
+            End Get
+            Set(ByVal value As String)
+                mUsuario = value
+            End Set
+        End Property
+        ''' <summary> 
+        ''' Password del Usuario de la BD. 
+        ''' </summary> 
+        Public Property Password() As String
+            Get
+                Return mPassword
+            End Get
+            Set(ByVal value As String)
+                mPassword = value
+            End Set
+        End Property
+
+        ''' <summary> 
+        ''' Cadena de conexión completa a la base. 
+        ''' </summary> 
+
+        Public MustOverride Property CadenaConexion() As String
+
+#End Region
+#Region "Privadas"
+        ''' <summary> 
+        ''' Crea u obtiene un objeto para conectarse a la base de dtaos. 
+        ''' </summary> 
+
+        Protected ReadOnly Property Conexion() As System.Data.IDbConnection
+            Get
+                If mConexion Is Nothing Then
+                    mConexion = CrearConexion(Me.CadenaConexion)
+                End If
+                If mConexion.State <> System.Data.ConnectionState.Open Then
+                    mConexion.Open()
+                End If
+                Return mConexion
+            End Get
+        End Property
+#End Region
+#Region "Lecturas"
+        ''' <summary> 
+        ''' Obtiene un DataSet a partir de un Procedimiento Almacenado. 
+        ''' </summary> 
+        Public Function TraerDataset(ByVal ProcedimientoAlmacenado As String) As System.Data.DataSet
+            Dim mDataSet As New System.Data.DataSet()
+            Me.CrearDataAdapter(ProcedimientoAlmacenado).Fill(mDataSet)
+            Return mDataSet
+
+        End Function
+        ''' <summary> 
+        ''' Obtiene un DataSet a partir de un Procedimiento Almacenado y sus parámetros. 
+        ''' </summary> 
+        Public Function TraerDataset(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As System.Data.DataSet
+            Dim mDataSet As New System.Data.DataSet()
+            Me.CrearDataAdapter(ProcedimientoAlmacenado, Args).Fill(mDataSet)
+            Return mDataSet
+        End Function
+        ''' <summary>
+        ''' Obtiene un DataSet a partir de un Procedimiento Almacenado y sus parámetros de entrada y salida
+        ''' </summary>
+        ''' <param name="ProcedimientoAlmacenado"></param>
+        ''' <param name="Parametros"></param>
+        ''' <param name="Args"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function TraerDataset(ByVal ProcedimientoAlmacenado As String, ByVal Parametros() As String, ByVal ParamArray Args As Object()) As System.Data.DataSet
+            Dim mDataSet As New System.Data.DataSet()
+            Me.CrearDataAdapter(ProcedimientoAlmacenado, Parametros, Args).Fill(mDataSet)
+            Return mDataSet
+        End Function
+        ''' <summary>
+        ''' Obtiene un DataSet a partir de un Query Sql.
+        ''' </summary>
+        ''' <param name="Consulta"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function TraerDatasetConsulta(ByVal Consulta As String) As System.Data.DataSet
+            Dim mDataSet As New System.Data.DataSet()
+            Me.CrearDataAdapterConsulta(Consulta).Fill(mDataSet)
+            Return mDataSet
+        End Function
+        ''' <summary> 
+        ''' Obtiene un DataTable a partir de un Procedimiento Almacenado. 
+        ''' </summary> 
+        Public Function TraerDataTable(ByVal ProcedimientoAlmacenado As String) As System.Data.DataTable
+            Return TraerDataset(ProcedimientoAlmacenado).Tables(0).Copy()
+        End Function
+        ''' <summary>
+        ''' btiene un DataTable a partir de un Query Sql.
+        ''' </summary>
+        ''' <param name="Consulta"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function TraerDataTableConsulta(ByVal Consulta As String) As System.Data.DataTable
+            Return TraerDatasetConsulta(Consulta).Tables(0).Copy()
+        End Function
+        ''' <summary> 
+        ''' Obtiene un DataSet a partir de un Procedimiento Almacenado y sus parámetros. 
+        ''' </summary> 
+        Public Function TraerDataTable(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As System.Data.DataTable
+            Return TraerDataset(ProcedimientoAlmacenado, Args).Tables(0).Copy()
+        End Function
+        ''' <summary>
+        ''' Obtiene un DataTable a partir de un Procedimiento Almacenado y sus parámetros de entrada y salida
+        ''' </summary>
+        ''' <param name="ProcedimientoAlmacenado"></param>
+        ''' <param name="Parametros"></param>
+        ''' <param name="Args"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function TraerDataTable(ByVal ProcedimientoAlmacenado As String, ByVal Parametros() As String, ByVal ParamArray Args As Object()) As System.Data.DataTable
+            Return TraerDataset(ProcedimientoAlmacenado, Parametros, Args).Tables(0).Copy()
+        End Function
+        ''' <summary> 
+        ''' Obtiene un DataReader a partir de un Procedimiento Almacenado. 
+        ''' </summary> 
+        Public Function TraerDataReader(ByVal ProcedimientoAlmacenado As String) As System.Data.IDataReader
+            Dim mDataSet As New System.Data.DataSet()
+            Me.CrearDataAdapter(ProcedimientoAlmacenado).Fill(mDataSet)
+            Return mDataSet.Tables(0).CreateDataReader()
+        End Function
+        ''' <summary> 
+        ''' Obtiene un DataReader a partir de un Procedimiento Almacenado y sus parámetros. 
+        ''' </summary> 
+        Public Function TraerDataReader(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As System.Data.IDataReader
+            Dim mDataSet As New System.Data.DataSet()
+            Me.CrearDataAdapter(ProcedimientoAlmacenado, Args).Fill(mDataSet)
+            Return mDataSet.Tables(0).CreateDataReader()
+        End Function
+        ''' <summary> 
+        ''' Obtiene un Valor a partir de un Procedimiento Almacenado. 
+        ''' </summary> 
+        Public Function TraerValor(ByVal ProcedimientoAlmacenado As String) As Object
+            Dim Com As System.Data.IDbCommand = Comando(ProcedimientoAlmacenado)
+            Com.ExecuteNonQuery()
+            Dim Resp As Object = Nothing
+            For Each Par As System.Data.IDbDataParameter In Com.Parameters
+                If Par.Direction = System.Data.ParameterDirection.InputOutput OrElse Par.Direction = System.Data.ParameterDirection.Output Then
+                    Resp = Par.Value
+                End If
+            Next
+            Return Resp
+        End Function
+
+        ''' <summary> 
+        ''' Obtiene un Valor Escalar a partir de un Procedimiento Almacenado. 
+        ''' </summary> 
+        Public Function TraerValorEscalar(ByVal ProcedimientoAlmacenado As String) As Object
+            Dim Com As System.Data.IDbCommand = Comando(ProcedimientoAlmacenado)
+            Return Com.ExecuteScalar()
+        End Function
+        ''' <summary> 
+        ''' Obtiene un Valor Escalar a partir de un Procedimiento Almacenado, con Params de Entrada
+        ''' </summary> 
+        Public Function TraerValorEscalar(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As Object
+            Dim Com As System.Data.IDbCommand = Comando(ProcedimientoAlmacenado)
+            CargarParametros(Com, Args)
+            Return Com.ExecuteScalar()
+        End Function
+
+        ''' <summary> 
+        ''' Obtiene un Valor Escalar a partir de un Procedimiento Almacenado, con Params de Entrada y parametros definidos
+        ''' </summary> 
+        Public Function TraerValorEscalar(ByVal ProcedimientoAlmacenado As String, ByVal Parametros() As String, ByVal ParamArray Args As Object()) As Object
+            Dim Com As System.Data.IDbCommand = Comando(ProcedimientoAlmacenado)
+            CargarParametros(Com, Parametros, Args)
+            Com.ExecuteScalar()
+            'retrono los valores de la consulta del procedimiento tambien con la cantidad de filas afectadas por defecto
+            Dim Par As System.Data.IDbDataParameter = DirectCast(Com.Parameters(0), System.Data.IDbDataParameter)
+            Return Par.Value
+        End Function
+
+        ''' <summary> 
+        ''' Obtiene un Valor a partir de un Procedimiento Almacenado, y sus parámetros. 
+        ''' </summary> 
+        Public Function TraerValor(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As Object
+            Dim Com As System.Data.IDbCommand = Comando(ProcedimientoAlmacenado)
+            CargarParametros(Com, Args)
+            Com.ExecuteNonQuery()
+            Dim Resp As Object = Nothing
+            For Each Par As System.Data.IDbDataParameter In Com.Parameters
+                If Par.Direction = System.Data.ParameterDirection.InputOutput OrElse Par.Direction = System.Data.ParameterDirection.Output OrElse Par.Direction = System.Data.ParameterDirection.ReturnValue Then
+                    Resp = Par.Value
+                End If
+            Next
+            Return Resp
+        End Function
+#End Region
+#Region "Acciones"
+        Protected MustOverride Function CrearConexion(ByVal Cadena As String) As System.Data.IDbConnection
+        Protected MustOverride Function Comando(ByVal ProcedimientoAlmacenado As String) As System.Data.IDbCommand
+        Protected MustOverride Function ComandoConsulta(ByVal Consulta As String) As System.Data.IDbCommand
+        Protected MustOverride Function CrearDataAdapter(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As System.Data.IDataAdapter
+        Protected MustOverride Function CrearDataAdapterConsulta(ByVal Consulta As String) As System.Data.IDataAdapter
+        Protected MustOverride Function CrearDataAdapter(ByVal ProcedimientoAlmacenado As String, ByVal Parametros() As String, ByVal ParamArray Args As Object()) As System.Data.IDataAdapter
+        Protected MustOverride Sub CargarParametros(ByVal Comando As System.Data.IDbCommand, ByVal Args As Object())
+        Protected MustOverride Sub CargarParametros(ByVal Comando As System.Data.IDbCommand, ByVal Parametros() As String, ByVal Args As Object())
+
+        ''' <summary> 
+        ''' Ejecuta un Procedimiento Almacenado en la base. 
+        ''' </summary> 
+        Public Function Ejecutar(ByVal ProcedimientoAlmacenado As String) As Integer
+            Return Comando(ProcedimientoAlmacenado).ExecuteNonQuery()
+        End Function
+
+        Public Function Autenticar() As Boolean
+            Try
+                If Me.Conexion.State <> System.Data.ConnectionState.Open Then
+                    Me.Conexion.Open()
+                End If
+                Return True
+            Catch ex As Exception
+                Return False
+                Throw New System.Exception(ex.Message)
+            End Try
+        End Function
+
+        ''' <summary> 
+        ''' Ejecuta un Procedimiento Almacenado en la base, utilizando los parámetros. 
+        ''' </summary> 
+        Public Function Ejecutar(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As Integer
+            Dim Com As System.Data.IDbCommand = Comando(ProcedimientoAlmacenado)
+            CargarParametros(Com, Args)
+            Dim Resp As Integer = Com.ExecuteNonQuery()
+
+            For i As Integer = 1 To Com.Parameters.Count - 1
+                'For i As Integer = 1 To Com.Parameters.Count - 1 yp,ofifique
+                'retrono los valores de la consulta del procedimiento tambien con la cantidad de filas afectadas por defecto
+                Dim Par As System.Data.IDbDataParameter = DirectCast(Com.Parameters(i), System.Data.IDbDataParameter)
+                If Par.Direction = System.Data.ParameterDirection.InputOutput OrElse Par.Direction = System.Data.ParameterDirection.Output Then
+                    Args.SetValue(Par.Value, i - 1)
+                    'Args.SetValue(Par.Value, i) yo modifique
+                End If
+            Next
+            Return Resp
+        End Function
+
+
+        Public Function EjecutarOutput(ByVal ProcedimientoAlmacenado As String, ByVal Parametros() As String, ByVal ParamArray Args As Object()) As Object()
+            Dim Com As System.Data.IDbCommand = Comando(ProcedimientoAlmacenado)
+            Dim Retorno(Parametros.Count - 1) As Object
+            CargarParametros(Com, Parametros, Args)
+            Dim Resp As Integer = Com.ExecuteNonQuery()
+
+            For i As Integer = 1 To Com.Parameters.Count - 1
+                'retrono los valores de la consulta del procedimiento tambien con la cantidad de filas afectadas por defecto
+                Dim Par As System.Data.IDbDataParameter = DirectCast(Com.Parameters(i), System.Data.IDbDataParameter)
+                If Par.Direction = System.Data.ParameterDirection.InputOutput OrElse Par.Direction = System.Data.ParameterDirection.Output OrElse Par.Direction = System.Data.ParameterDirection.ReturnValue Then
+                    'Args.SetValue(Par.Value, i - 1)
+                    For c As Integer = 0 To Parametros.Length - 1
+                        If Par.ParameterName = Parametros(c) Then
+                            Retorno.SetValue(Par.Value, c)
+                        End If
+                    Next
+                End If
+            Next
+
+            Return Retorno
+        End Function
+
+#End Region
+
+#Region "Transacciones"
+        Protected mTransaccion As System.Data.IDbTransaction
+        Protected EnTransaccion As Boolean = False
+        ''' <summary> 
+        ''' Comienza una Transacción en la base en uso. 
+        ''' </summary> 
+        Public Sub IniciarTransaccion()
+            mTransaccion = Me.Conexion.BeginTransaction()
+            EnTransaccion = True
+        End Sub
+        ''' <summary> 
+        ''' Confirma la transacción activa. 
+        ''' </summary> 
+        Public Sub TerminarTransaccion()
+            Try
+                mTransaccion.Commit()
+            Catch Ex As System.Exception
+                Throw Ex
+            Finally
+                mTransaccion = Nothing
+                EnTransaccion = False
+            End Try
+        End Sub
+        ''' <summary> 
+        ''' Cancela la transacción activa. 
+        ''' </summary> 
+        Public Sub AbortarTransaccion()
+            Try
+                mTransaccion.Rollback()
+            Catch Ex As System.Exception
+                Throw Ex
+            Finally
+                mTransaccion = Nothing
+                EnTransaccion = False
+            End Try
+        End Sub
+#End Region
+    End Class
+End Namespace
+
---
+++ DatosSQLServer.vb
@@ -0,0 +1,209 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Text
+
+Namespace Datos
+    Public Class DatosSQLServer
+        Inherits DatosGlobales.DatosGlobales
+        Shared ColComandos As New System.Collections.Hashtable()
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <value></value>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Overloads Overrides Property CadenaConexion() As String
+            Get
+                If Me.mCadenaConexion.Length = 0 Then
+                    If Me.mBase.Length <> 0 AndAlso Me.mServidor.Length <> 0 Then
+                        Dim sCadena As New System.Text.StringBuilder("")
+                        sCadena.Append("data source=<SERVIDOR>;")
+                        sCadena.Append("initial catalog=<BASE>;password=<PASSWORD>;")
+                        sCadena.Append("persist security info=True;")
+                        sCadena.Append("user id=<USER>;packet size=4096")
+                        sCadena.Replace("<SERVIDOR>", Me.Servidor)
+                        sCadena.Replace("<BASE>", Me.Base)
+                        sCadena.Replace("<USER>", Me.Ususario)
+                        sCadena.Replace("<PASSWORD>", Me.Password)
+                        Return sCadena.ToString()
+                    Else
+                        Dim Ex As New System.Exception("No se puede establecer la cadena de conexión")
+                        Throw Ex
+                    End If
+                End If
+                Return Nothing
+            End Get
+            Set(ByVal value As String)
+                Me.mCadenaConexion = value
+            End Set
+        End Property
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="Com"></param>
+        ''' <param name="Args"></param>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Sub CargarParametros(ByVal Com As System.Data.IDbCommand, ByVal Args As Object())
+            'Dim Limite As Integer = Com.Parameters.Count
+            For i As Integer = 1 To Com.Parameters.Count - 1
+                Dim P As System.Data.SqlClient.SqlParameter = DirectCast(Com.Parameters(i), System.Data.SqlClient.SqlParameter)
+                If i <= Args.Length Then
+                    P.Value = Args(i - 1)
+                Else
+                    P.Value = Nothing
+                End If
+            Next
+        End Sub
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="Com"></param>
+        ''' <param name="Parametros"></param>
+        ''' <param name="Args"></param>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Sub CargarParametros(ByVal Com As System.Data.IDbCommand, ByVal Parametros() As String, ByVal Args As Object())
+            Dim band As Boolean = False
+            Dim c As Integer = 1
+
+            While c <= Com.Parameters.Count - 1
+                Dim P As System.Data.SqlClient.SqlParameter = DirectCast(Com.Parameters(c), System.Data.SqlClient.SqlParameter)
+                For i As Integer = 0 To Parametros.Length - 1
+                    If P.ParameterName = Parametros(i) Then
+                        band = True
+                        P.Value = (Args.GetValue(i))
+                        'c = c + 1
+                        Exit For
+                    End If
+                Next
+                If band Then
+                    band = False
+                Else
+                    P.Value = Nothing
+                    'Com.Parameters.Remove(P)
+                End If
+                c = c + 1
+            End While
+        End Sub
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="ProcedimientoAlmacenado"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Function Comando(ByVal ProcedimientoAlmacenado As String) As System.Data.IDbCommand
+
+            Dim Com As System.Data.SqlClient.SqlCommand
+            If ColComandos.Contains(ProcedimientoAlmacenado) Then
+                Com = DirectCast(ColComandos(ProcedimientoAlmacenado), System.Data.SqlClient.SqlCommand)
+            Else
+                Dim Con2 As New System.Data.SqlClient.SqlConnection(Me.CadenaConexion)
+                Con2.Open()
+                Com = New System.Data.SqlClient.SqlCommand(ProcedimientoAlmacenado, Con2)
+                Com.CommandType = System.Data.CommandType.StoredProcedure
+                System.Data.SqlClient.SqlCommandBuilder.DeriveParameters(Com)
+                Con2.Close()
+                Con2.Dispose()
+
+                ColComandos.Add(ProcedimientoAlmacenado, Com)
+            End If
+            Com.Connection = DirectCast(Me.Conexion, System.Data.SqlClient.SqlConnection)
+            Com.Transaction = DirectCast(Me.mTransaccion, System.Data.SqlClient.SqlTransaction)
+            Return DirectCast(Com, System.Data.IDbCommand)
+        End Function
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="Consulta"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Function ComandoConsulta(ByVal Consulta As String) As System.Data.IDbCommand
+            Dim Com As New System.Data.SqlClient.SqlCommand
+            Com.CommandType = System.Data.CommandType.Text
+            Com.CommandText = Consulta
+            Com.Connection = DirectCast(Me.Conexion, System.Data.SqlClient.SqlConnection)
+            Return DirectCast(Com, System.Data.IDbCommand)
+        End Function
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="CadenaConexion"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Function CrearConexion(ByVal CadenaConexion As String) As System.Data.IDbConnection
+            Return DirectCast(New System.Data.SqlClient.SqlConnection(CadenaConexion), System.Data.IDbConnection)
+        End Function
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="ProcedimientoAlmacenado"></param>
+        ''' <param name="Args"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Function CrearDataAdapter(ByVal ProcedimientoAlmacenado As String, ByVal ParamArray Args As Object()) As System.Data.IDataAdapter
+            Dim Da As New System.Data.SqlClient.SqlDataAdapter(DirectCast(Comando(ProcedimientoAlmacenado), System.Data.SqlClient.SqlCommand))
+            Da.SelectCommand.CommandTimeout = 200
+            If Args.Length <> 0 Then
+                CargarParametros(Da.SelectCommand, Args)
+            End If
+            Return DirectCast(Da, System.Data.IDataAdapter)
+        End Function
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="Consulta"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Function CrearDataAdapterConsulta(ByVal Consulta As String) As System.Data.IDataAdapter
+            Dim Da As New System.Data.SqlClient.SqlDataAdapter(DirectCast(ComandoConsulta(Consulta), System.Data.SqlClient.SqlCommand))
+            Da.SelectCommand.CommandTimeout = 200
+            Return DirectCast(Da, System.Data.IDataAdapter)
+        End Function
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="ProcedimientoAlmacenado"></param>
+        ''' <param name="Parametros"></param>
+        ''' <param name="Args"></param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Protected Overloads Overrides Function CrearDataAdapter(ByVal ProcedimientoAlmacenado As String, ByVal Parametros() As String, ByVal ParamArray Args As Object()) As System.Data.IDataAdapter
+            Dim Da As New System.Data.SqlClient.SqlDataAdapter(DirectCast(Comando(ProcedimientoAlmacenado), System.Data.SqlClient.SqlCommand))
+            Da.SelectCommand.CommandTimeout = 200
+            If Args.Length <> 0 Then
+                CargarParametros(Da.SelectCommand, Parametros, Args)
+            End If
+            Return DirectCast(Da, System.Data.IDataAdapter)
+        End Function
+
+        Public Sub New()
+            ' 
+            ' TODO: agregar aquí la lógica del constructor 
+            ' 
+            Me.Base = ""
+            Me.Servidor = ""
+            Me.Ususario = ""
+            Me.Password = ""
+        End Sub
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="CadenaConexion"></param>
+        ''' <remarks></remarks>
+        Public Sub New(ByVal CadenaConexion As String)
+            Me.CadenaConexion = CadenaConexion
+        End Sub
+
+        Public Sub New(ByVal Servidor As String, ByVal Base As String)
+            Me.Base = Base
+            Me.Servidor = Servidor
+        End Sub
+
+        Public Sub New(ByVal Servidor As String, ByVal Base As String, ByVal User As String, ByVal Password As String)
+            Me.Base = Base
+            Me.Servidor = Servidor
+            Me.Ususario = User
+            Me.Password = Password
+        End Sub
+
+    End Class
+End Namespace
